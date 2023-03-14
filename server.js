382e9de54df2ef901a19a0381102c4ada6bc9a8d
Sabrina Poderis <act172@einstein.com>
2023-03-14 19:45:50 -0300

    feat: make socket work

--- server.js
+++ server.js
@@ -1,8 +1,15 @@
 const app = require('express')()
 const http = require('http').createServer(app);
+const io = require("socket.io")(http);
 
-const io = require('socket.io')(http)
+app.get('/', (req,res) => {
+  res.sendFile(`${__dirname}/index.html`)
+})
+
+io.on("connection", (socket) => {
+  console.log('👤 Client connection: ', socket)
+});
 
 http.listen(3000, () => {
-  console.log('🧦 Socket server listening on port 3000')  
+  console.log('🧦 Socket server listening on http://localhost:3000')  
 })
